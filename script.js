415e9e67dd0ee169976dccb86121069d962c90c3
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-10-17 10:50:33 +0000

    commit1

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
