6bf3472f11cef5d5ebda82630af9bfb5f5fe5445
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-10-21 11:33:38 +0000

    pereguntasorganizadas

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ const perguntas = [
   {
     enunciado:
       "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-    alternativas: ["Isso é assustador!", "Isso é maravilhoso!"],
+    alternativas: [{
+
+        texto: "Isso é assustador!",
+        afirmação: "afirmação",
+
+        texto: "Isso é maravilhoso!",
+        afirmação: "afirmação"}],
   },
   {
     enunciado:
       "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
     alternativas: [
-      "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-      "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-    ],
+        {
+        texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+        afirmacao: "afirmação",
+        
+        texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+        afirmacao: "afirmação"
+    }],
   },
   {
     enunciado:
@@ -48,13 +58,17 @@ let perguntaAtual;
 function mostraAlternativa(){
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+          atual++;
+          mostraPergunta();  
+        });
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
 
 function mostraPergunta(){
-    perguntaAtual = pergunta[atual];
+    perguntaAtual = perguntas[atual];
     caixaPergunta.textContent = perguntaAtual.enunciado;
     mostraAlternativa();
 }
